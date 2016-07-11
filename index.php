a6e72794ad31e035e5ca4488e3effdca7ddb02cd
mindlabs <mindlabs@mindlabs-System-Product-Name>
2016-07-11 09:48:22 +0530

    second commit

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-     echo "Hihiiiii";
+     echo "Hihiiiii"; 
+     echo "second edit";
         ?>
     </body>
 </html>
